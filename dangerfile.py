fa601057aad449aa49ebb0ad85ae51f9e500c700
Jakub Turek <jkbturek@gmail.com>
2020-02-14 15:21:03 +0100

    Update dangerfile.py

--- dangerfile.py
+++ dangerfile.py
@@ -1,4 +1,4 @@
 title = danger.github.pr.title
-warn(title)
-message(f"Modified {len(danger.git.modified_files)} files in the PR")
-fail(f"{danger.github.pr.user.login} says hello to the Hackathon demo")
+message(title)
+#warn(f"Modified {len(danger.git.modified_files)} files in the PR")
+#fail(f"{danger.github.pr.user.login} says hello to the Hackathon demo")
